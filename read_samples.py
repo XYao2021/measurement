9d1d6ba6e08c545cc5a9ffde10c44af5a93a58d9
Xin Yao <90070559+XYao2021@users.noreply.github.com>
2024-01-24 10:07:43 -0700

    Add files via upload

--- read_samples.py
+++ read_samples.py
@@ -1,6 +1,9 @@
 import numpy as np
 import scipy.signal as sig
+import pandas as pd
 import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+import os
 
 def bandpass_filter(samps, fcenter, fpass, srate, order=5):
     '''
@@ -10,44 +13,55 @@ def bandpass_filter(samps, fcenter, fpass, srate, order=5):
     srate: The sample rate
     '''
     nyq = 0.5*srate
-    shift_seq = np.exp(-2j * np.pi * fcenter / srate * np.arange(len(samps)))
+    shift_seq = np.exp(-2j * np.pi * fcenter / srate * np.arange(len(samps)))  # multiple e^(-j*2*pi*fc*Ts*n) in time = frequency shift
     shift_samps = samps * shift_seq
-    b, a = sig.butter(order, fpass/nyq, btype='low')
-    fsamps =  sig.lfilter(b,a, shift_samps)
+    b, a = sig.butter(N=order, Wn=fpass/nyq, btype='low')
+    fsamps = sig.lfilter(b, a, shift_samps)
     return fsamps / shift_seq
 
+def parse_args():
+    """Parse the command line arguments"""
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-t", "--target", help="target base station", type=str, default=None)
+    return parser.parse_args()
 
-data = np.load('/data/samples_20240123-163653/20240123-163654.832801-IQrate220000.000000_nsamples131072.npy')
-data = data[0]
+
+args = parse_args()
+folders = ['data/'+name for name in os.listdir('data/') if name.startswith('samples_20')]
 
 tx_separation = 27.5e3 + 13.75e3  # From Serhat's slides
 
 transmitter_frequencies = [tx_separation*-2, tx_separation*-1, 0, tx_separation*1, tx_separation*2]
-# print(transmitter_frequencies)
-
 # EBC dense node was replaced with the USTAR rooftop node due to interference starting on Wed 28th
 names = ['ebc', 'guesthouse', 'mario', 'moran', 'ustar']
-sines = {}
 filter_width = 7e3
 shift_fudge = 5e3
 
-for name, fcenter in zip(names, transmitter_frequencies):
-    sines[name] = bandpass_filter(data, fcenter+shift_fudge, filter_width, 220e3, order=7)
+data = {}
+for folder in folders:
+    for file in os.listdir(folder):
+        time = pd.to_datetime(file.split('-IQ')[0].split('.')[0])
+        time = np.datetime64(time).astype('datetime64[s]')
+        data[time] = np.load(os.path.join(folder, file))[0]
+        # sines = {}
+        # for name, fcenter in zip(names, transmitter_frequencies):
+        #     sines[name] = bandpass_filter(data[time], fcenter + shift_fudge, filter_width, 220e3, order=7)
+times = sorted(list(data.keys()))
+print(data, '\n')
+# print(times)
+fig, axs = plt.subplots(1, 2)
 
-## To compare the PSD (in other words, see the filtered signals)
-## Decreasing Filter width will give you a cleaner signal, at risk of missing your actual signal if the fudge factor or center freq is off.
-## Fudge factor may not be constant for all data?
-for label in sines:
-    sine = sines[label]
-    plt.plot(np.fft.fftshift(np.fft.fftfreq(len(data), 1/220e3)), 10.0 * np.log10(abs(np.fft.fftshift(np.fft.fft(sine)))), label=label)
-    plt.xlabel("Frequency + 3.5 GHz")
-plt.legend()
-plt.show()
+def animate(i):
+    print(times[i], "%i/%i" % (i, len(times)), end='\r')
+    axs[0].clear()
+    axs[0].plot(3.534e9 + np.fft.fftshift(np.fft.fftfreq(len(data[times[i]]), 1/0.22e6)), 10.0 * np.log10(abs(np.fft.fftshift(np.fft.fft(data[times[i]])))))
+    axs[0].set_ylim(-20, 40)
 
-##  To see the filtered sinusoids:
-# for label in sines:
-#     sine = sines[label]
-#     plt.plot(abs(sine), label=label)
-#     plt.xlabel("Time")
-# plt.legend()
+#     axs[1].clear()
+#     axs[1].scatter(longitude, latitude, marker='.', )
+#     if times[i] in coords:
+#         axs[1].scatter(*coords[times[i]], marker='*', s=100)
+#
+#
+# ani = FuncAnimation(fig, animate, frames=len(times), interval=1, repeat=True)
 # plt.show()
